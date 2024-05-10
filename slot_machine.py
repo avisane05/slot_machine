aacf9ffa4d2ae3a3f0aedc0bdd4ab73a245b6b3b
avisane05 <avisane05.as@gmail.com>
2024-05-10 11:54:43 +0530

    code 1

--- slot_machine.py
+++ slot_machine.py
@@ -1,6 +1,38 @@
+import random
+
 MAX_LINE = 3
 MIN_BET = 1
 MAX_BET = 100
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[], [], []]
+
+    for _ in range(cols):
+        column = []
+        currunt_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(currunt_symbols)
+            currunt_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
 
 def deposit():
     while True:
@@ -48,7 +80,17 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    print(balance, lines, bet)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your currunt balance is: Rs.{balance}")
+        else:
+            break
+
+    print(f"You are betting Rs.{bet} on {lines}. Total bet is equal to: RS.{total_bet}")
+
+    
 
 main()
